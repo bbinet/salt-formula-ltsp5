e6359df20a4a63e89fd1b20e75f08187a92c7e94
Bruno Binet <bruno.binet@gmail.com>
2018-09-07 12:30:32 +0000

    nm_unmanage_device.py: do nothing when host dbus socket does not exist

--- ltsp/files/nm_unmanage_device.py
+++ ltsp/files/nm_unmanage_device.py
@@ -13,8 +13,13 @@ import os
 
 import dbus
 
+DBUS_SOCKET_PATH="/host/run/dbus/system_bus_socket"
 
-os.environ["DBUS_SYSTEM_BUS_ADDRESS"] = "unix:path=/host/run/dbus/system_bus_socket"
+if not os.access(DBUS_SOCKET_PATH, os.R_OK and os.W_OK):
+    # do nothing when host dbus socket does not exist
+    sys.exit(0)
+
+os.environ["DBUS_SYSTEM_BUS_ADDRESS"] = "unix:path=%s" % DBUS_SOCKET_PATH
 
 device = sys.argv[1]
 bus = dbus.SystemBus()
